6e9de37252dd65da7f6b070d932e461e6191e834
Joshua Smith <joshuamsmith413@gmail.com>
2019-02-11 21:42:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo St
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
  function theBeatlesPlay(musicians, instruments) {
    let answer = []
-   for (let i = 0; i > muscians.length; i++) {
+   for (let i = 0; i > musicians.length; i++) {
      answer.push(musicians[i] + 'plays' + instruments[i])
    }
    return answer;
